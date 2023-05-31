5a8c730205d80813cf799fb3b5b843ecd9ca9b19
Nana Kwame <ynkwame@gmail.com>
2023-05-31 16:19:11 +0000

    Added explore page and navigations

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,7 +2,6 @@ import { SearchBar } from "./SearchBar";
 import { BiCameraMovie } from "react-icons/bi";
 
 const NavBar = () => {
-  // const [toggleDropDown, setToggleDropDown] = useState(false);
   return (
     <div>
       <nav className="bg-gray-800 px-8 w-full flex h-10 text-white items-center gap-6 justify-between">
@@ -12,30 +11,6 @@ const NavBar = () => {
         <div className="p-3 m-5 flex items-center justify-center gap-7 max-w-2xl w-full text-black">
           <SearchBar />
         </div>
-
-        {/* <div className="flex">
-          <div className="bg-white relative flex flex-col m-5 justify-end">
-            <div className="flex">
-              <div className="bg-green-500 text-white w-[2rem]">I</div>
-              <div
-                className=" text-black mx-3 px-1 hover:cursor-pointer mb-0 font-semibold"
-                onClick={() => setToggleDropDown((prev) => !prev)}
-              >
-                UPLOADS
-              </div>
-            </div>
-            {toggleDropDown && (
-              <div className="absolute top-6 w-full">
-                <div className="bg-white text-black border-2 border-black-400 h-10 flex justify-center items-center cursor-pointer">
-                  Single
-                </div>
-                <div className="bg-white text-black h-10 border-2 border-black-400 flex justify-center items-center cursor-pointer">
-                  Album
-                </div>
-              </div>
-            )}
-          </div>
-        </div> */}
 
         <div className=" flex justify-center items-center w-[10rem] h-full m-5">
           <div className="bg-white text-black w-[1.7rem] h-[1.7rem] rounded-full flex items-center justify-center ">
---
+++ src/pages/ExploreMovie.jsx
@@ -0,0 +1,70 @@
+import React, { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+
+const ExploreMovie = () => {
+  const [movie, setMovie] = useState([]);
+  let { movieId } = useParams();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const options = {
+        method: "GET",
+        headers: {
+          "X-RapidAPI-Key":
+            "bca07a01aemshc113c7875dd1d6bp1795f8jsn2f8e6671d464",
+          "X-RapidAPI-Host": "online-movie-database.p.rapidapi.com",
+        },
+      };
+
+      try {
+        const response = await fetch(
+          `https://online-movie-database.p.rapidapi.com/title/get-details?tconst=${movieId}`,
+          options
+        );
+        const result = await response.json();
+        console.log(result);
+        console.log("ok");
+        setMovie(result);
+      } catch (error) {
+        console.error(error);
+        console.log("not ok", movieId);
+      }
+    };
+    fetchData();
+  }, [movieId]);
+  return (
+    <div>
+      <button
+        className="bg-blue-600 p-3 mx-10 mt-2 text-white hover:bg-blue-800"
+        onClick={() => navigate("/home")}
+      >
+        Go Home
+      </button>
+      <div className="w-full h-screen flex items-center justify-center">
+        <div className="w-[25rem] bg-gray-400 h-[35rem] flex flex-col justify-between items-center rounded-3xl hover:-translate-y-3 hover:shadow-2xl duration-500 cursor-pointer">
+          <img
+            src={`${movie?.image?.url}`}
+            alt="pro"
+            className="bg-red-600 w-3/4 h-3/4 mt-2 rounded-lg"
+          />
+          <div className="bg-gray-600 w-full h-28 flex flex-col items-center justify-center rounded-b-3xl">
+            <h2 className="font-bold text-xl text-center px-1">
+              {movie?.title}
+            </h2>
+            <h3 className="font-lighter">Show Type:{movie?.titleType}</h3>
+            {movie?.year ? (
+              <span>Year of release: {movie.year}</span>
+            ) : (
+              <span>
+                Years of show: {movie?.seriesStartYear}-{movie?.seriesEndYear}
+              </span>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ExploreMovie;
